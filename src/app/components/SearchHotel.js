6491370566d4e372550ddad055dd123bcface2d5
mdsultanmahmud <mdsultanmahmud.bd00@gmail.com>
2023-06-04 20:07:07 +0600

    in the banner section tour search option done

--- src/app/components/SearchHotel.js
+++ src/app/components/SearchHotel.js
@@ -1,9 +1,64 @@
 import React from 'react';
-
-const SearchHotel = ({search}) => {
+import commonStyle from '../styles/common.module.css'
+import styles from '../styles/banner.module.css'
+const SearchHotel = ({ search }) => {
     return (
         <div className={`${search == 'hotel' ? 'block' : 'hidden'}`}>
-            <h1>Search here for hotel</h1>
+            <div className={`grid gap-x-5 grid-cols-4`}>
+                <div className={`${styles.tourContent} ${styles.hotelContent}`}>
+                    <label htmlFor='tour'>CITY/HOTEL/RESORT/AREA</label>
+                    <select id='tour'>
+                        <option defaultChecked>
+                            Cox Bazar
+                            <span>Bangladesh</span>
+                        </option>
+                        <option>Sajek</option>
+                        <option>Tanguar Haor</option>
+                        <option>Cox Bazar</option>
+                        <option>Sajek</option>
+                        <option>Tanguar Haor</option>
+                        <option>Cox Bazar</option>
+                        <option>Sajek</option>
+                        <option>Tanguar Haor</option>
+                    </select>
+                </div>
+                <div className={`${styles.tourContent} ${styles.hotelContent}`}>
+                    <label htmlFor='tour'>CHECK IN</label>
+                    <input type='date' />
+                </div>
+                <div className={`${styles.tourContent} ${styles.hotelContent}`}>
+                    <label htmlFor='tour'>CHECK OUT</label>
+                    <input type='date' />
+                </div>
+                <div className={`${styles.tourContent} ${styles.hotelContent}`}>
+                    <label htmlFor='tour'>ROOMS & GUESTS</label>
+                    <h2>{1} Room, {2} Guests</h2>
+                    <p>2 Adults</p>
+                </div>
+            </div>
+            <div className={`flex gap-x-8 items-center ${styles.selectionContainer}`}>
+                <h4>Search For</h4>
+                <div>
+                    <input id='business' type='checkbox' />
+                    <label htmlFor='business'>Business</label>
+                </div>
+                <div>
+                    <input id='Couples' type='checkbox' />
+                    <label htmlFor='Couples'>Couples</label>
+                </div>
+                <div>
+                    <input id='Families' type='checkbox' />
+                    <label htmlFor='Families'>Families</label>
+                </div>
+                <div>
+                    <input id='Friends' type='checkbox' />
+                    <label htmlFor='Friends'>Friends</label>
+                </div>
+                <div>
+                    <input id='Solo' type='checkbox' />
+                    <label htmlFor='Solo'>Solo</label>
+                </div>
+            </div>
         </div>
     );
 };
